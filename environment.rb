d40f391198505b36128657477c4ac5e12b9a97c1
akolesnikov <akolesnikov@MBP26C2D-02.local>
2010-05-13 14:49:32 -0400

    Initial commit

---
+++ environment.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-validations'
+require 'dm-aggregates'
+require 'haml'
+require 'ostruct'
+
+require 'sinatra' unless defined?(Sinatra)
+
+configure do
+  SiteConfig = OpenStruct.new(
+                 :title => 'Nodecasts.com',
+                 :author => 'Andrew Kolesnikov',
+                 :url_base => 'http://nodecasts.com/'
+               )
+
+  DataMapper.setup(:default, ENV['DATABASE_URL'] ||  "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db")
+
+  # load models
+  $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+  Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
+end
